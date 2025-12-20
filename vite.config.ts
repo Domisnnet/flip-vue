b175b7132f3d70ba3570422511643b24cf143206
dominique.goncalves61 <dominique.goncalves61@gmail.com>
2025-12-20 16:53:57 +0000

    ok

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  //base: '/Shadow-Flip-Oh-Vue/', // Será necessário deixar comentado para alteração de build , na opção para Firebase Hosting
+  base: '/Shadow-Flip-Oh-Vue/', // Será necessário deixar comentado para alteração de build , na opção para Firebase Hosting
   plugins: [
     vue(),
   ],
